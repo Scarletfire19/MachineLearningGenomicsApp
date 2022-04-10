f69b4d1067f205ee93ad0f3e35ecfbec061b385c
Scarletfire19 <89076186+Scarletfire19@users.noreply.github.com>
2022-04-11 03:42:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ import io
 import numpy as np
 import pandas as pd
 
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.title("Machine Learning Genomics App")
 
 

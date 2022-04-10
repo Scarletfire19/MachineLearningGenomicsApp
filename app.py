2dcbfdff694286903ecd85c4bb245bf38b673160
Scarletfire19 <89076186+Scarletfire19@users.noreply.github.com>
2022-04-11 00:48:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,19 +32,6 @@ dfancienthpg=dfancientpcadna.groupby(['Assigned Mutation']).mean().reset_index()
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
-
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-     st.write(stringio)
-
-     # To read file as string:
-     string_data = stringio.read()
-     st.write(string_data)
-
-     # Can be used wherever a "file-like" object is accepted:
      input = pd.read_csv(uploaded_file)
      st.write(input)
     
